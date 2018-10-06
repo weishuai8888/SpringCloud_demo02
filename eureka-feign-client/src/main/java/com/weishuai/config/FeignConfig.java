3c100f7e272bfe8b94f39a8c40529d779f0f5d10
weiShuai <601770050@qq.com>
2018-10-06 20:36:41 +0800

    新增注释

--- eureka-feign-client/src/main/java/com/weishuai/config/FeignConfig.java
+++ eureka-feign-client/src/main/java/com/weishuai/config/FeignConfig.java
@@ -11,7 +11,8 @@ import java.util.concurrent.TimeUnit;
  */
 @Configuration //表明是一个配置类
 public class FeignConfig {
-    @Bean
+    @Bean//注入Retryer的Bean后，Feign在远程调用失败后会进行重试
+    //下面的代码，通过覆盖默认的Retryer的Bean，更改了FeignClient的请求失败重试策略，重试间隔为100毫秒，最大重试时间为1秒，重试次数为5次。
     public Retryer feignRetryer(){
         return new Retryer.Default(100, TimeUnit.SECONDS.toMillis(1), 5);
     }
