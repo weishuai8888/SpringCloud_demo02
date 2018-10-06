127c352f38c3ae1caf16e3d9f721318a18bf762e
weiShuai <601770050@qq.com>
2018-10-06 22:24:05 +0800

    使用Hystrix Dashboard监控熔断器的状态（端口号不匹配，没成功）

--- eureka-ribbon-client/src/main/java/com/weishuai/EurekaRibbonClientApplication.java
+++ eureka-ribbon-client/src/main/java/com/weishuai/EurekaRibbonClientApplication.java
@@ -4,15 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * 服务消费者
  * 使用RestTemplate和Ribbon来消费服务
  * 在RestTemplate和Ribbon上使用熔断器
+ * 使用Hystrix Dashboard监控熔断器的状态（端口号不匹配，没成功）
  */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableHystrixDashboard
 public class EurekaRibbonClientApplication {
 
 	public static void main(String[] args) {
